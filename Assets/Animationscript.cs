c589c6cd2f59af879232e2849937b472ed6c6ab3
Link949 <2154324@talnet.nl>
2022-12-22 11:18:53 +0100

    Animatie player af!
    klopt

--- Assets/Animationscript.cs
+++ Assets/Animationscript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class Animationscript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+    private bool grounded;
+
+  
+    private void Awake()
     {
-        
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,9 +20,27 @@ public class Animationscript : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
+        
+
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
+
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -17,7 +17,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.LeftShift))
         {
             TakeDamage(20);
         }
